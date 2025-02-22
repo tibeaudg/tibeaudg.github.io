d25b8affce2ad51ff132702712199e928d6fff41
tibeaudg <tibeaudegrauwe@gmail.com>
2025-02-22 13:42:35 +0100

    Start vragen toegevoegd (+50)

--- pages/game/questions.tsx
+++ pages/game/questions.tsx
@@ -1,16 +1,238 @@
-// questions.ts
-
 export const questions = [
-    {
-      question: 'What is 2 + 2?',
-      options: ['3', '4', '5'],
-      correctAnswer: '4',
-    },
-    {
-      question: 'What is the capital of France?',
-      options: ['Berlin', 'Madrid', 'Paris'],
-      correctAnswer: 'Paris',
-    },
-    // Add more questions here
-  ];
-  
+  {
+    question: 'Wie is de hoofdrolspeler in de film "Aladdin"?',
+    options: ['Aladdin', 'Jafar', 'Genie'],
+    correctAnswer: 'Aladdin',
+  },
+  {
+    question: 'Wat is de naam van de leeuw in "The Lion King"?',
+    options: ['Simba', 'Scar', 'Mufasa'],
+    correctAnswer: 'Simba',
+  },
+  {
+    question: 'Welke Disney prinses heeft een glazen schoen?',
+    options: ['Sneeuwwitje', 'Cinderella', 'Aurora'],
+    correctAnswer: 'Cinderella',
+  },
+  {
+    question: 'Wat is de naam van de blauwe genie in "Aladdin"?',
+    options: ['Genie', 'Iago', 'Abu'],
+    correctAnswer: 'Genie',
+  },
+  {
+    question: 'Welke Disney film speelt zich af in de stad New Orleans?',
+    options: ['De Prinses en de Kikker', 'Rapunzel', 'Mulan'],
+    correctAnswer: 'De Prinses en de Kikker',
+  },
+  {
+    question: 'Wie is de grootste vijand van Mulan?',
+    options: ['Shan Yu', 'Hades', 'Scar'],
+    correctAnswer: 'Shan Yu',
+  },
+  {
+    question: 'Wat is de naam van de vis in "Finding Nemo"?',
+    options: ['Dory', 'Marlin', 'Bruce'],
+    correctAnswer: 'Dory',
+  },
+  {
+    question: 'Welke film speelt zich af in de Arctische regio?',
+    options: ['Frozen', 'Mulan', 'Moana'],
+    correctAnswer: 'Frozen',
+  },
+  {
+    question: 'Welke Disney film bevat de liedjes "Let It Go" en "Do You Want to Build a Snowman?"',
+    options: ['Frozen', 'Cinderella', 'Beauty and the Beast'],
+    correctAnswer: 'Frozen',
+  },
+  {
+    question: 'Wat is de naam van de olifant in "Dumbo"?',
+    options: ['Dumbo', 'Bambi', 'Thumper'],
+    correctAnswer: 'Dumbo',
+  },
+  {
+    question: 'Wie is de vrouwelijke hoofdrolspeler in "Mulan"?',
+    options: ['Mulan', 'Aurora', 'Jasmine'],
+    correctAnswer: 'Mulan',
+  },
+  {
+    question: 'Wat is de naam van het hondje in "The Lady and the Tramp"?',
+    options: ['Lady', 'Tramp', 'Pluto'],
+    correctAnswer: 'Lady',
+  },
+  {
+    question: 'In welke film verschijnt de personage "Rapunzel"?',
+    options: ['Frozen', 'The Little Mermaid', 'Tangled'],
+    correctAnswer: 'Tangled',
+  },
+  {
+    question: 'Wat is de naam van de tovenaar in "Fantasia"?',
+    options: ['Mickey Mouse', 'Merlijn', 'Mufasa'],
+    correctAnswer: 'Mickey Mouse',
+  },
+  {
+    question: 'Wat is de naam van het kasteel in "Beauty and the Beast"?',
+    options: ['Het Beestenhuis', 'Het Witte Huis', 'Het Kasteel van de Rose'],
+    correctAnswer: 'Het Beestenhuis',
+  },
+  {
+    question: 'Wie is de boze stiefmoeder in "Sneeuwwitje"?',
+    options: ['Maleficent', 'Ursula', 'De Stiefmoeder'],
+    correctAnswer: 'De Stiefmoeder',
+  },
+  {
+    question: 'Wat is de naam van de vis in "Finding Nemo" die altijd vergeet? ',
+    options: ['Marlin', 'Dory', 'Nemo'],
+    correctAnswer: 'Dory',
+  },
+  {
+    question: 'Wat is de naam van de slang in "Jungle Book"?',
+    options: ['Kaa', 'Shere Khan', 'Baloo'],
+    correctAnswer: 'Kaa',
+  },
+  {
+    question: 'Wat is de naam van de hond in "101 Dalmatiërs"?',
+    options: ['Pongo', 'Bolt', 'Pluto'],
+    correctAnswer: 'Pongo',
+  },
+  {
+    question: 'Welke Disney-prinses had een betoverde roos?',
+    options: ['Belle', 'Ariel', 'Rapunzel'],
+    correctAnswer: 'Belle',
+  },
+  {
+    question: 'Wie is de schurk in de film "Hercules"?',
+    options: ['Hades', 'Zeus', 'Ares'],
+    correctAnswer: 'Hades',
+  },
+  {
+    question: 'Wie is de beste vriend van Simba in "The Lion King"?',
+    options: ['Timon', 'Zazu', 'Scar'],
+    correctAnswer: 'Timon',
+  },
+  {
+    question: 'Wat is de naam van de vader van Simba in "The Lion King"?',
+    options: ['Mufasa', 'Scar', 'Zazu'],
+    correctAnswer: 'Mufasa',
+  },
+  {
+    question: 'Welke Disney-prinses is in een toren opgesloten?',
+    options: ['Rapunzel', 'Ariel', 'Jasmine'],
+    correctAnswer: 'Rapunzel',
+  },
+  {
+    question: 'Wie is de koning van de zee in "The Little Mermaid"?',
+    options: ['Koning Triton', 'Sebastian', 'Flounder'],
+    correctAnswer: 'Koning Triton',
+  },
+  {
+    question: 'Welke Disney film heeft een liedje getiteld "Under the Sea"?',
+    options: ['The Little Mermaid', 'Aladdin', 'Frozen'],
+    correctAnswer: 'The Little Mermaid',
+  },
+  {
+    question: 'Wat is de naam van de blauwe tangvis in "Finding Nemo"?',
+    options: ['Dory', 'Nemo', 'Marlin'],
+    correctAnswer: 'Dory',
+  },
+  {
+    question: 'Wie is de slechte stiefmoeder in "Cinderella"?',
+    options: ['De stiefmoeder', 'Maleficent', 'De Heks'],
+    correctAnswer: 'De stiefmoeder',
+  },
+  {
+    question: 'Wie is de grootste vijand van Peter Pan?',
+    options: ['Kapitein Haak', 'Peter Pan', 'Tinker Bell'],
+    correctAnswer: 'Kapitein Haak',
+  },
+  {
+    question: 'Wie is het favoriete dier van Elsa in "Frozen"?',
+    options: ['Rendier Sven', 'Hondje Pluto', 'Pinguïn'],
+    correctAnswer: 'Rendier Sven',
+  },
+  {
+    question: 'Wat is de naam van het meisje in "Brave"?',
+    options: ['Merida', 'Ariel', 'Elsa'],
+    correctAnswer: 'Merida',
+  },
+  {
+    question: 'Welke Disney-prinses is een vis?',
+    options: ['Ariel', 'Mulan', 'Cinderella'],
+    correctAnswer: 'Ariel',
+  },
+  {
+    question: 'Wie is de beste vriend van Rapunzel in "Tangled"?',
+    options: ['Pascal', 'Flynn', 'Maximus'],
+    correctAnswer: 'Pascal',
+  },
+  {
+    question: 'In welke film komt de personage "Bolt" voor?',
+    options: ['Bolt', 'Aladdin', 'Bambi'],
+    correctAnswer: 'Bolt',
+  },
+  {
+    question: 'Wat is de naam van de kleine draak in "Mulan"?',
+    options: ['Mushu', 'Feng', 'Shan Yu'],
+    correctAnswer: 'Mushu',
+  },
+  {
+    question: 'Wie is de prins in "Sneeuwwitje"?',
+    options: ['De prins', 'Eric', 'Charming'],
+    correctAnswer: 'De prins',
+  },
+  {
+    question: 'Welke Disney-figuur is een zelfverzekerde rat die koken leert?',
+    options: ['Remy', 'Aladdin', 'Dory'],
+    correctAnswer: 'Remy',
+  },
+  {
+    question: 'Wat is de naam van de prinses in "Moana"?',
+    options: ['Moana', 'Rapunzel', 'Ariel'],
+    correctAnswer: 'Moana',
+  },
+  {
+    question: 'Welke Disney prinses heeft een betoverd bed? ',
+    options: ['Aurora', 'Cinderella', 'Jasmine'],
+    correctAnswer: 'Aurora',
+  },
+  {
+    question: 'Wie zijn de drie slechte heksen in "Hocus Pocus"?',
+    options: ['Witches', 'Sanderson Sisters', 'Witches of Oz'],
+    correctAnswer: 'Sanderson Sisters',
+  },
+  {
+    question: 'Wie was de "onmogelijke" prinses in de Disney film?',
+    options: ['Belle', 'Tiana', 'Mulan'],
+    correctAnswer: 'Tiana',
+  },
+  {
+    question: 'Wie is de assistent van de boze koning in "Cinderella"?',
+    options: ['Lucifer', 'Jafar', 'Cruella'],
+    correctAnswer: 'Lucifer',
+  },
+  {
+    question: 'Welke Disney film bevat een slang genaamd Kaa?',
+    options: ['Jungle Book', 'The Lion King', 'The Little Mermaid'],
+    correctAnswer: 'Jungle Book',
+  },
+  {
+    question: 'Wat is de naam van de geheime stad in "Atlantis: The Lost Empire"?',
+    options: ['Atlantis', 'Mordor', 'El Dorado'],
+    correctAnswer: 'Atlantis',
+  },
+  {
+    question: 'Wat is de naam van de bad guy in "Toy Story"?',
+    options: ['Sid', 'Woody', 'Buzz'],
+    correctAnswer: 'Sid',
+  },
+  {
+    question: 'Wie is de grootste vijand van Elsa in "Frozen 2"?',
+    options: ['Rune', 'Hades', 'The Dark Spirit'],
+    correctAnswer: 'The Dark Spirit',
+  },
+  {
+    question: 'In welke film komt de personage "Gaston" voor?',
+    options: ['Beauty and the Beast', 'Cinderella', 'The Little Mermaid'],
+    correctAnswer: 'Beauty and the Beast',
+  },
+  // Voeg hier meer vragen toe
+];
