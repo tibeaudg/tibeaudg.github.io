1745b84b8fe9eeb66a09a800e12dbd5fd4643958
tibeaudg <tibeaudegrauwe@gmail.com>
2025-02-27 21:46:44 +0100

    Update friends.tsx

--- pages/friends.tsx
+++ pages/friends.tsx
@@ -223,7 +223,7 @@ const handleInvite = async (e: React.FormEvent) => {
 
       <header className="header">
         <div className="logo">
-          <Image src="/assets/logo.png" alt="Logo" width={100} height={100} />
+          <Image src="/assets/Magic Quest.png" alt="Logo" width={100} height={100} />
         </div>
           <div className="hamburger-menu" onClick={toggleMenu}>
           <div className="hamburger-icon"></div>
